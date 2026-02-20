93b5e434f4501fc5a60da151ed8df7c05d4265fc
Yash Tandon <ytandon27@students.polytechnic.org>
2026-02-19 22:45:15 -0800

    I add a new Project

---
+++ palindrome_checker.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+__author__ = "Yash Tandon"
+__version__ = "2026-02-18"
+
+from atds import Deque
+
+
+class PalindromeChecker:
+
+    def __init__(self):
+        self.strict_mode = False
+
+    def set_strict_mode(self, value):
+        self.strict_mode = value
+
+    def sanitize(self, phrase):
+        cleaned = ""
+        for ch in phrase:
+            if ch.isalpha():
+                cleaned += ch.lower()
+        return cleaned
+
+    def is_palindrome(self, phrase):
+
+        if not self.strict_mode:
+            phrase = self.sanitize(phrase)
+
+        # Must contain at least one character
+        if len(phrase) == 0:
+            return False
+
+        d = Deque()
+
+        for ch in phrase:
+            d.add_rear(ch)
+
+        while d.size() > 1:
+            front = d.remove_front()
+            rear = d.remove_rear()
+
+            if front != rear:
+                return False
+
+        return True
+
+
+def main():
+    checker = PalindromeChecker()
+
+    print("Palindrome Checker!")
+
+    mode = input("Do you want strict mode 1) on, or 2) off? --> ")
+
+    if mode == "1":
+        checker.set_strict_mode(True)
+    else:
+        checker.set_strict_mode(False)
+
+    phrase = input("Phrase: ")
+
+    if checker.is_palindrome(phrase):
+        print("Is a palindrome.")
+    else:
+        print("Not a palindrome.")
+
+
+if __name__ == "__main__":
+    main()
