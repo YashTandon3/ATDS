c0f883da2054872339589eebe2d1130ef9b9d622
Yash Tandon <ytandon27@students.polytechnic.org>
2026-02-24 08:34:22 -0800

    I made the node class.

--- atds.py
+++ atds.py
@@ -3,7 +3,7 @@
 __author__ = "Yash Tandon"
 __version__ = "2026-02-12"
 
-class Stack:
+class Stack():
     def __init__(self):
         self.stack = []
 
@@ -25,7 +25,7 @@ class Stack:
         return len(self.stack) == 0
 
 
-class Queue:
+class Queue():
     def __init__(self):
         self.queue = []
 
@@ -81,6 +81,23 @@ class Deque:
     
     def is_empty(self):
         return len(self.deque) == 0
+
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    def get_data(self):
+        return self.data
+    def set_data(self, new_data):
+        self.data = new_data
+    def get_next(self):
+        return self.next
+    def set_next(self, new_next):
+        self.next = new_next
+    def __repr__(self):
+        return f"Node[data={self.data}]" + f"next={self.next}"
+
 
 
 
