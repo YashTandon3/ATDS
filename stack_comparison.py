e8b7d77d62be3e3e7aa55c2db2ea3e647ba62e5c
Yash Tandon <ytandon27@students.polytechnic.org>
2026-03-10 22:04:58 -0700

    Para

--- stack_comparison.py
+++ stack_comparison.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
 __author__ = "Yash Tandon"
-__version__ = "2026-03-10"
+__version__ = "2026-03-1"
 
 from atds import *
 import time
 import matplotlib.pyplot as plt
 
+""""
+For this project, I had to measue the time it took to push
+and pop items from the stack and unorderled list classes.
+In each stack, I tested sizes from 100,000 to 1,000,000
+while recording the time it took to push and pull items 
+onto and off the stack. After graphing and visualzing the 
+data, I found that the list stack was much faster than 
+the unordered list stack. This difference is most likely due
+to they ways that the 2 stacks use their memory. Lists are more
+straightforward, appending elements more effiently, while the 
+unordered list has to take extra steps to keep node connection.
+Finally, both stacks are able to handle large numbers, but certain
+operations are more efficient on the list stack.
+
+"""""
 
 def testStackPush(s, n):
     start = time.time()
