109feae9ee69d058c0a3b2fda3c6dd08d8132ae9
Yash Tandon <ytandon27@students.polytechnic.org>
2026-04-16 14:09:26 -0700

    I do my evaluate function

--- parse_tree.py
+++ parse_tree.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 __author__ = "Yash Tandon"
 __date__ = "2026-04-14"
-import token
 
 from atds import *
 
 
-def build_parse_tree(tokens : list) -> BinaryTree:
+def build_parse_tree(tokens: list) -> BinaryTree:
     bt = BinaryTree(None)
     st = Stack()
     current = bt
@@ -16,7 +15,7 @@ def build_parse_tree(tokens : list) -> BinaryTree:
             current.insert_left(None)
             st.push(current)
             current = current.get_left_child()
-        elif token in ('+-/*'):
+        elif token in ('+', '-', '*', '/'):  # FIX: use tuple, not string
             current.set_root_val(token)
             current.insert_right(None)
             st.push(current)
@@ -27,15 +26,31 @@ def build_parse_tree(tokens : list) -> BinaryTree:
             current.set_root_val(int(token))
             current = st.pop()
     return bt
-       
-    
+
+
+def evaluate(parse_tree: BinaryTree):
+    operators = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b,
+    }
+
+    left = parse_tree.get_left_child()
+    right = parse_tree.get_right_child()
+
+    if left and right:
+        op = operators[parse_tree.get_root_val()]
+        return op(evaluate(left), evaluate(right))
+    else:
+        return parse_tree.get_root_val()
 
 
 def main():
     fpe = "( 2 * 13 )"
     tokens = fpe.split(" ")
     bt = build_parse_tree(tokens)
-    print(bt)
+    print(evaluate(bt))
 
 if __name__ == "__main__":
     main()
